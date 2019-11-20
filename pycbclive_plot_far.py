413813304abeb9181308bce6be2afe0508079608
Tito Dal Canton <tito.canton@ligo.org>
2019-11-20 02:58:07 -0800

    Make pycbclive_plot_far.py more generic

--- pycbclive_plot_far.py
+++ pycbclive_plot_far.py
@@ -1,21 +1,32 @@
-import sys
+#!/usr/bin/env python
+
+"""Make a plot to verify the rate of false alarms from PyCBC Live."""
+
+import argparse
 import glob
 import h5py
 import tqdm
 import lal
 import numpy as np
+import matplotlib
+matplotlib.use('agg')
 import pylab as pl
 from scipy.stats import poisson
 
 
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument('--input-files', required=True)
+parser.add_argument('--output-plot', required=True)
+args = parser.parse_args()
+
 ifars = []
 stats = []
-upload_thresholds = []
-pvalue_livetimes = []
-dfuts = []
+upload_thresholds = set()
+pvalue_livetimes = set()
+dfuts = set()
 time = 0.
 
-for fn in tqdm.tqdm(glob.glob(sys.argv[1])):
+for fn in tqdm.tqdm(glob.glob(args.input_files)):
     with h5py.File(fn, 'r') as f:
         if 'num_live_detectors' not in f.attrs:
             continue
@@ -36,11 +47,11 @@ for fn in tqdm.tqdm(glob.glob(sys.argv[1])):
             if i == 0:
                 continue
             if cl[i-1] == '--ifar-upload-threshold':
-                upload_thresholds.append(float(arg))
+                upload_thresholds.add(float(arg))
             elif cl[i-1] == '--pvalue-combination-livetime':
-                pvalue_livetimes.append(float(arg))
+                pvalue_livetimes.add(float(arg))
             elif cl[i-1] == '--ifar-double-followup-threshold':
-                dfuts.append(float(arg))
+                dfuts.add(float(arg))
 
 ifars = np.sort(np.array(ifars))
 count = np.arange(len(ifars))[::-1] + 1
@@ -58,21 +69,22 @@ for prob in [0.6827, 0.9545, 0.9973]:
                     edgecolor='none', facecolor='C1', label=label)
     label = None
 
-for ut in set(upload_thresholds):
+for ut in upload_thresholds:
     pl.axvline(ut, color='r', ls='--', label='--ifar-upload-threshold')
 
-for pvlt in set(pvalue_livetimes):
+for pvlt in pvalue_livetimes:
     pl.axvline(pvlt, color='b', ls=':', label='--pvalue-combination-livetime')
 
-for dfut in set(dfuts):
-    pl.axvline(dfut, color='g', ls='-.', label='--ifar-double-followup-threshold')
+for dfut in dfuts:
+    pl.axvline(dfut, color='g', ls='-.',
+               label='--ifar-double-followup-threshold')
 
 pl.xscale('log')
 pl.yscale('log')
 pl.xlabel('Inverse FAR [yr]')
 pl.ylabel('Cumulative rate [yr$^{-1}$]')
-pl.title(sys.argv[1], fontsize=10)
+pl.title(args.input_files, fontsize=10)
 pl.legend(fontsize=10)
 
 pl.tight_layout()
-pl.savefig('/home/tito/public_html/pycbc/live/o3_initial_review/trigger_rate_replay.png', dpi=200)
+pl.savefig(args.output_plot, dpi=200)
