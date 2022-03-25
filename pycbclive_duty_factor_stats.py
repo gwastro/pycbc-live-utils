4367d7ab6e20d041ca164a91ef954455b4565345
Tito Dal Canton <tito.dalcanton@ijclab.in2p3.fr>
2022-03-25 14:25:08 +0100

    Add script for showing duty factor stats

---
+++ pycbclive_duty_factor_stats.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+"""Read the duty factors from a PyCBC Live log and show some stats about them."""
+
+import argparse
+import numpy as np
+import matplotlib.pyplot as pp
+
+
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument('--input-log', required=True)
+parser.add_argument('--out-plot')
+args = parser.parse_args()
+
+duty_factors = {}
+
+with open(args.input_log, 'r') as lf:
+    for line in lf:
+        if 'Took' not in line:
+            continue
+        pieces = line.split()
+        rank = int(pieces[3])
+        duty_factor = float(pieces[9].replace(',', ''))
+        if rank not in duty_factors:
+            duty_factors[rank] = []
+        duty_factors[rank].append(duty_factor)
+
+print('Duty factors:')
+for rank in sorted(duty_factors):
+    print('{:04d}: {:d} samples, median {:.2f}, 90th percentile {:.2f}'.format(
+        rank,
+        len(duty_factors[rank]),
+        np.median(duty_factors[rank]),
+        np.percentile(duty_factors[rank], 90)
+    ))
+
+if args.out_plot is not None:
+    df_max = 0
+    df_min = np.inf
+    for rank in sorted(duty_factors):
+        color = pp.cm.viridis(rank / max(duty_factors))
+        lw = 2 if rank == 0 else 0.5
+        pp.hist(
+            duty_factors[rank],
+            10000,
+            cumulative=-1,
+            density=True,
+            histtype='step',
+            color=color,
+            lw=lw
+        )
+        df_max = max(df_max, max(duty_factors[rank]))
+        df_min = min(df_min, min(duty_factors[rank]))
+
+    pp.axvspan(
+        1, df_max, hatch='/', facecolor='none', edgecolor='gray'
+    )
+    pp.xscale('log')
+    pp.xlim(max(df_min, 0.1), df_max)
+    pp.ylim(0, 1)
+    pp.xlabel('Duty factor')
+    pp.ylabel('Cumulative fraction')
+    pp.title('Median at rank 0: {:.2f} {}'.format(
+        np.median(duty_factors[0]),
+        '\N{CHECK MARK}' if np.median(duty_factors[0]) < 1 else '\N{WARNING SIGN}'
+    ))
+
+    pp.tight_layout()
+    pp.savefig(args.out_plot, dpi=200)
